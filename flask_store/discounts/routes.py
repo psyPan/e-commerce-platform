3f442856ee89ac9eee384d4a01dd69200160a97f
Jeff <t112590038@ntut.org.tw>
2025-12-14 23:08:43 +0800

    Remove turtle import from discounts.routes and update home page

--- flask_store/discounts/routes.py
+++ flask_store/discounts/routes.py
@@ -1,4 +1,3 @@
-from turtle import title
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import current_user, login_required
 from flask_store import db
