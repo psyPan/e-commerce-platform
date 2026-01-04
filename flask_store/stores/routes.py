42c724b166b105d749fb94974ef035eecb84161f
Jeff <t112590038@ntut.org.tw>
2026-01-04 14:28:43 +0800

    Deleted old html files

--- flask_store/stores/routes.py
+++ flask_store/stores/routes.py
@@ -184,7 +184,7 @@ def product_detail(store_id, product_id):
         Product.stock > 0
     ).limit(4).all()
     
-    return render_template('old/product_details.html',
+    return render_template('common/product_details.html',
                            store=store,
                            product=product,
                            related_products=related_products,
